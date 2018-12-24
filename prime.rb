af96706d3623f9e22694649a1254fd54d4b0bad0
Maximo Bautista <max.bautista97@gmail.com>
2018-12-24 04:22:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,7 +6,7 @@ def prime?(number)
     isPrime = true
     half = number / 2
     for int in 2..half
-      if number % int == 0 
+      if number < 2 || number % int == 0 
         isPrime = false
         break
       end
