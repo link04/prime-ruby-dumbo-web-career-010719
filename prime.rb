cca475f363ba71482fb278125852f00963a8ac46
Maximo Bautista <max.bautista97@gmail.com>
2018-12-24 03:28:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+  
+  if number < 2
+    false
+  else
+    i = 2
+    root = number **(1.0/3)
+     for i..root 
+      if number % i === 0
+        return false;
+    return num !== 1 && num !== 0;
+  end
+    
+end
+  
