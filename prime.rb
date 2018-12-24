061eeb470e75a74a88967f712f52c272658602c0
Maximo Bautista <max.bautista97@gmail.com>
2018-12-24 04:26:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+  if  number < 2
+    isPrime = false
+  else
+    isPrime = true
+    half = number / 2
+    for int in 2..half
+      if number % int == 0 
+        isPrime = false
+        break
+      end
+    end
+  end
+    isPrime
+end
+  
